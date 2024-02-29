ea6b3fc4f1534d3a1ff90f437602d8800c424724
jhonatanAlexis <84157823+jhonatanAlexis@users.noreply.github.com>
2024-02-28 18:21:27 -0600

    Delete singleton directory

--- singleton/Controller.java
+++
@@ -1,8 +0,0 @@
-package com.anahuac.software.creacional.singleton;
-
-public class Controller {
-    public static void main(String[] args){
-        LoggerSingleton ls = LoggerSingleton.getInstance("Justificante");
-        ls.log();
-    }
-}
